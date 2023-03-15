2edb61476ad7fe6af519bfd92dfc5da19f8f2bdf
something <danielyanartur0@gmail.com>
2023-03-15 19:13:50 +0300

    TodoReact

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoFunction from './Components/TodoFunction';
+
+function App() {
+  return (
+ <TodoFunction/>
+  );
+}
+
+export default App;
---
+++ src/Components/ToDoItem.js
@@ -0,0 +1,36 @@
+const ToDoItem = (props) => {
+  const { toDo, onDelClick, onEditBtn, onEditToDoNameChange } = props;
+  return (
+    <div>
+      {toDo.editable == true ? (
+        <input
+          onChange={(e) => props.onEditToDoNameChange(toDo.id, e.target.value)}
+          value={toDo.name}
+        />
+      ) : (
+       <>
+       <div>{toDo.name}{1}
+       <button>Red</button> 
+        </div>
+      </> 
+        
+      )}
+
+      <button
+        onClick={() => {
+          onDelClick(toDo.id);
+        }}
+      >
+        Del
+      </button>
+      <button
+        onClick={() => {
+          onEditBtn(toDo.id);
+        }}
+      >
+        {toDo.editable ? "Save" : "Edit"}
+      </button>
+    </div>
+  );
+};
+export default ToDoItem;
